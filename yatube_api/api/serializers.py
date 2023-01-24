c7c0bb3b68af82b77bb583ac07fcf7a2cfa99ad8
valentine <valentin9304@yandex.ru>
2023-01-24 22:42:27 +0700

    второй

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -27,7 +27,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         fields = '__all__'
-        read_only_fields = ['post',]
+        read_only_fields = ['post', ]
         model = Comment
 
 
